37e09166072152762c626602ab4b18d517daef95
hendisantika <hendisantika@yahoo.co.id>
2024-11-09 16:03:04 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/jtetask/SpringBootJteTaskApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jtetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJteTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJteTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/jtetask/SpringBootJteTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jtetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJteTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
