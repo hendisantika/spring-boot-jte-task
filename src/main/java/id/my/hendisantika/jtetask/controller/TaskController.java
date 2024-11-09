fe0bc89ab55dcf4e9a0789ef9f4334cd66af610c
hendisantika <hendisantika@yahoo.co.id>
2024-11-09 16:09:15 +0700

    Add index in TaskController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/jtetask/controller/TaskController.java
+++ src/main/java/id/my/hendisantika/jtetask/controller/TaskController.java
@@ -4,6 +4,8 @@ import id.my.hendisantika.jtetask.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +24,9 @@ public class TaskController {
 
     private final TaskRepository taskRepository;
 
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("tasks", taskRepository.findAll());
+        return "index";
+    }
 }
